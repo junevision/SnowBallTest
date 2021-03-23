99efe8467602873007c8b87d6f11a45c41618df6
Jun_Lei <v-jlei@hotels.com>
2021-03-23 11:51:04 +0800

    add test json with mitmproxy

---
+++ test_mock/map_local.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Mingjun Lei
+@file: map_local.py
+@time: 2021/3/23 10:20
+@desc: Basic skeleton of a mitmproxy addon.
+"""
+from mitmproxy import ctx, http
+
+
+class Junevision:
+
+    def request(self, flow: http.HTTPFlow):
+        """
+        use request event to complete map local
+        :param flow:
+        :return:
+        """
+        if "https://stock.xueqiu.com/v5/stock/batch/quote.json?_t=" in flow.request.pretty_url:
+            with open("/Users/jun_lei/Documents/Jun/code/python/SnowBallTest/test_mock/test.json",
+                      encoding="utf-8") as f:
+                # assign value to flow.response attribute
+                # call mitmproxy response object make function
+                # the data in make function in response body is str
+                flow.response = http.HTTPResponse.make(200,  # (optional) status code
+                                                       f.read(),  # (optional) content
+                                                       {"Content-Type": "text/html"}  # (optional) headers
+                                                       )
+
+    def response(self, flow: http.HTTPFlow):
+        pass
+
+
+# addons is a must name for mitmproxy, store instance
+addons = [
+    Junevision()
+]
---
+++ test_mock/rewrite.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Mingjun Lei
+@file: rewrite.py
+@time: 2021/3/23 10:15
+@desc: Basic skeleton of a mitmproxy addon.
+"""
+import json
+from mitmproxy import ctx, http
+
+
+class June:
+
+    def request(self, flow):
+        pass
+
+    def response(self, flow: http.HTTPFlow):
+        # verify url request, whether it includes url info
+        if "https://stock.xueqiu.com/v5/stock/batch/quote.json?_t=" in flow.request.pretty_url:
+            # receive response data info
+            # flow.response.text is str attribute
+            # str -> python json
+            data = json.loads(flow.response.text)
+            percents_items = [0, 0.1, -0.1, -100, 100, 1000]
+            for item in range(len(percents_items)):
+                data["data"]["items"][item]["quote"]["percent"] = percents_items[item]
+                print(item)
+            # python json -> str
+            flow.response.text = json.dumps(data)
+            print(flow.response.text)
+
+
+# addons is a must name for mitmproxy, store instance
+addons = [
+    June()
+]
